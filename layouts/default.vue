16f6b9c439051770edfa3871a2429f935acc2eea
田畑颯 <chinarra.tabata@gmail.com>
2018-12-02 12:51:06 +0900

    Create footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,22 @@
 <template lang="pug">
-  nuxt
+  div
+    nuxt
+    div(id="footer")
+      b-container
+        b-row(class="justify-content-md-center text-center")
+          b-col
+            h2 ShokuTaku
+        b-row(class="justify-content-md-center text-center")
+          b-col
+            nuxt-link(class="link" to="/terms/privacy") privacy policy
 </template>
+
+<style lang="stylus">
+#footer
+  margin-top 40px
+  padding 30px 0
+  background-color #E7E7E7
+  color #757575
+  .link
+    color #757575
+</style>
